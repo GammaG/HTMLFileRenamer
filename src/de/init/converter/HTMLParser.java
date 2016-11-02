b5b0f36f949678cbafd8864585a544f7bf5c8427
mseidler <marco.seidler@outlook.com>
2016-11-02 16:28:48 +0100

    test removed

--- src/de/init/converter/HTMLParser.java
+++ src/de/init/converter/HTMLParser.java
@@ -5,8 +5,6 @@ import de.init.usage.Constants;
 public class HTMLParser {
 
 	public String replaceDesignContent(String htmlFile) {
-		boolean test = htmlFile.contains(Constants.REGEX);
-
 		return htmlFile.replaceAll(Constants.REGEX, Constants.REPLACEMENT);
 	}
 
